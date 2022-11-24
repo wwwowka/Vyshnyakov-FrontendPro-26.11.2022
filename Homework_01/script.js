f5c721e4225b564c2c12420471c6d1e9b40d0869
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-11-24 16:25:14 +0200

    homework 1

---
+++ Homework_01/script.js
@@ -0,0 +1,3 @@
+let userName;
+userName = prompt('What is your name?', 'Your name');
+alert("Hello, " + userName + " How are you?");
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hillel-Frontend-Pro
+## Homework #1
+We use variables, strings, string concatenation, prompt and alert commands.

7729280e23b9bf42cc214d0f166c02d28e0ba043
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-11-26 12:17:26 +0200

    mentor's edits

--- Homework_01/script.js
+++ Homework_01/script.js
@@ -1,3 +1,7 @@
-let userName = "";
+/* let userName = "";
 userName = prompt("What is your name?", "Your name");
+alert("Hello, " + userName + " How are you?"); */
+
+//mentor:
+const userName = prompt("What is your name?", "Your name");
 alert("Hello, " + userName + " How are you?");

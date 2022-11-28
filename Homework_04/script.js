28e82aa6dce833806af10b7b0bddcbeba3c4f61f
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-11-28 21:47:31 +0200

    homework 4

--- Homework_04/script.js
+++ Homework_04/script.js
@@ -1,7 +1,7 @@
 const operator = prompt("Enter one of: add, sub, mult, div.", "add");
 const firstNumber = prompt("Enter the first number.", "123");
 const secondNumber = prompt("Enter the second number.", "123");
-const addValue = firstNumber + secondNumber;
+const addValue = +firstNumber + +secondNumber;
 const subValue = firstNumber - secondNumber;
 const multValue = firstNumber * secondNumber;
 const divValue = firstNumber / secondNumber;
@@ -14,11 +14,11 @@ switch (operator) {
         alert(`${firstNumber}-${secondNumber}=${subValue}`);
       break;
     case "mult":
-        alert(`${firstNumber}-${secondNumber}=${multValue}`);
+        alert(`${firstNumber}*${secondNumber}=${multValue}`);
       break;
     case "div":
-        alert(`${firstNumber}-${secondNumber}=${divValue}`);
+        alert(`${firstNumber}/${secondNumber}=${divValue}`);
       break;
     default:
-      console.log(`Sorry, you entered an invalid value.`);
+      alert(`Sorry, you entered an invalid value.`);
   }

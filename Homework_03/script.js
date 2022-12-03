91ca7c94af41bfd94d326d4868374ead0338b136
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-12-03 14:04:57 +0200

    fix h to s.

--- Homework_03/script.js
+++ Homework_03/script.js
@@ -1,5 +1,5 @@
 const hourValue = prompt("Enter the number of hours.", "12");
-const ratio = 360;
+const ratio = 3600;
 const secValue = hourValue * ratio;
 
 alert(`${hourValue}h = ${secValue}s.`);

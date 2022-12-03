9ce2c296367690d67e4eecfd219bfe2c105cc783
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-12-03 17:12:49 +0200

    homework 05 Accessing a string as an object.

--- Homework_05/script.js
+++ Homework_05/script.js
@@ -5,10 +5,13 @@ const thirdString = prompt("Enter the third string.", "Quae, magnam!");
 alert(`${secondString} ${firstString} ${thirdString}`);
 
 const enteredNumber = prompt("Enter five-digit number.", 98765);
-const digitFifth  = enteredNumber%10;
+
+/* const digitFifth  = enteredNumber%10;
 const digitFourth  = ((enteredNumber-enteredNumber%10)/10)%10;
 const digitThird  = ((enteredNumber-enteredNumber%100)/100)%10;  
 const digitSecond = ((enteredNumber-enteredNumber%1000)/1000)%10; 
 const digitFirst = (enteredNumber-enteredNumber%10000)/10000;
-
 alert(`${digitFirst} ${digitSecond} ${digitThird} ${digitFourth} ${digitFifth}`)
+ */
+
+alert(`${enteredNumber[0]} ${enteredNumber[1]} ${enteredNumber[2]} ${enteredNumber[3]} ${enteredNumber[4]}`);

038af16bb5cf27a93d89562db0d63e13409dc9cb
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-12-25 17:33:23 +0200

    function doMath

--- Homework_14/script.js
+++ Homework_14/script.js
@@ -13,3 +13,38 @@ function calcArithmeticMean(arr) {
 }
 
 console.log(calcArithmeticMean(arrayDifTypes));
+
+function doMath(x, znak, y) {
+  let result = 0;
+
+  switch (znak) {
+    case '+':
+      result = x + y;
+      break;
+    case '-':
+      result = x - y;
+      break;
+    case '*':
+      result = x * y;
+      break;
+    case '/':
+      result = x / y;
+      break;
+    case '%':
+      result = (x * y) / 100;
+      break;
+    case '^':
+      result = x ** y;
+      break;
+    default:
+      break;
+  }
+
+  return result;
+}
+
+const userNumX = +prompt('Введіть перше число');
+const userZnak = prompt('Введіть знак');
+const userNumY = +prompt('Введіть друге число');
+
+console.log(doMath(userNumX, userZnak, userNumY));

aa8e7b536842765eb021be6a56eeb74957cf05cb
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-12-31 12:01:45 +0200

    fix hw04_calc

--- Homework_04/script.js
+++ Homework_04/script.js
@@ -1,24 +1,20 @@
-const operator = prompt("Enter one of: add, sub, mult, div.", "add");
-const firstNumber = prompt("Enter the first number.", "123");
-const secondNumber = prompt("Enter the second number.", "123");
-const addValue = +firstNumber + +secondNumber;
-const subValue = firstNumber - secondNumber;
-const multValue = firstNumber * secondNumber;
-const divValue = firstNumber / secondNumber;
+const operator = prompt('Enter one of: add, sub, mult, div.', 'add');
+const firstNumber = prompt('Enter the first number.', '123');
+const secondNumber = prompt('Enter the second number.', '123');
 
 switch (operator) {
-    case "add":
-        alert(`${firstNumber}+${secondNumber}=${addValue}`);
-      break;
-    case "sub":
-        alert(`${firstNumber}-${secondNumber}=${subValue}`);
-      break;
-    case "mult":
-        alert(`${firstNumber}*${secondNumber}=${multValue}`);
-      break;
-    case "div":
-        alert(`${firstNumber}/${secondNumber}=${divValue}`);
-      break;
-    default:
-      alert(`Sorry, you entered an invalid value.`);
-  }
+  case 'add':
+    alert(`${firstNumber}+${secondNumber}=${+firstNumber + +secondNumber}`);
+    break;
+  case 'sub':
+    alert(`${firstNumber}-${secondNumber}=${firstNumber - secondNumber}`);
+    break;
+  case 'mult':
+    alert(`${firstNumber}*${secondNumber}=${firstNumber * secondNumber}`);
+    break;
+  case 'div':
+    alert(`${firstNumber}/${secondNumber}=${firstNumber / secondNumber}`);
+    break;
+  default:
+    alert('Sorry, you entered an invalid value.');
+}

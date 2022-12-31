91160242d9d50e6d4f4aa322705343f55274bdf2
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-12-31 11:54:37 +0200

    fix hw04_calc

--- Homework_04/script_v1.js
+++ Homework_04/script_v1.js
@@ -2,7 +2,7 @@ const operator = prompt('Enter one of: add, sub, mult, div.', 'add');
 const firstNumber = prompt('Enter the first number.', '123');
 const secondNumber = prompt('Enter the second number.', '123');
 
-switch (operator) {
+/* switch (operator) {
   case 'add':
     alert(`${firstNumber}+${secondNumber}=${+firstNumber + +secondNumber}`);
     break;
@@ -13,8 +13,19 @@ switch (operator) {
     alert(`${firstNumber}*${secondNumber}=${firstNumber * secondNumber}`);
     break;
   case 'div':
-    alert(`${firstNumber}/${secondNumber}=${firstNumber / secondNumber}`);
+    alert(`${firstNumber}/${secondNumber}=${irstNumber / secondNumber}`);
     break;
   default:
     alert('Sorry, you entered an invalid value.');
+} */
+
+// Closures
+
+function countTwoNumbers(oper) {
+  return function (first, second) {
+    console.log(first + oper + second);
+  };
 }
+
+const operCount = countTwoNumbers(operator);
+const result = operCount(firstNumber, secondNumber);

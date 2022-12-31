195d31f12a29fbd10c5a3889fdc627dbe8aa8ecc
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-12-31 12:05:48 +0200

    Merge branch 'main' of https://github.com/wwwowka/Hillel-Frontend-Pro

---
+++ Homework_14/script.js
@@ -0,0 +1,90 @@
+const arrayDifTypes = [5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78];
+
+function calcArithmeticMean(arr) {
+  let sumNum = 0;
+  let countNum = 0;
+  for (const el of arr) {
+    if (typeof el === 'number') {
+      sumNum += el;
+      countNum += 1;
+    }
+  }
+  return (sumNum / countNum);
+}
+
+console.log(calcArithmeticMean(arrayDifTypes));
+
+function doMath(x, znak, y) {
+  let result = 0;
+
+  switch (znak) {
+    case '+':
+      result = x + y;
+      break;
+    case '-':
+      result = x - y;
+      break;
+    case '*':
+      result = x * y;
+      break;
+    case '/':
+      result = x / y;
+      break;
+    case '%':
+      result = (x * y) / 100;
+      break;
+    case '^':
+      result = x ** y;
+      break;
+    default:
+      break;
+  }
+
+  return result;
+}
+
+const userNumX = +prompt('Введіть перше число');
+const userZnak = prompt('Введіть знак');
+const userNumY = +prompt('Введіть друге число');
+
+console.log(doMath(userNumX, userZnak, userNumY));
+
+const userArrayLength = +prompt('Введіть довжину масиву');
+const userSubarrayLength = +prompt('Введіть довжину підмасиву');
+
+function made2dArray(m, n) {
+  const arr = new Array(m);
+  for (let i = 0; i < m; i += 1) {
+    arr[i] = new Array(n);
+  }
+  return arr;
+}
+
+function fillArray(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < arr[i].length; j += 1) {
+      arr[i][j] = prompt(`Введіть [${i}][${j}] елемент массиву текст або число`);
+    }
+  }
+  return arr;
+}
+
+console.log(fillArray(made2dArray(userArrayLength, userSubarrayLength)));
+
+const arreyString = [...(prompt('Введіть слова'))];
+const arrayDelSymbol = [...(prompt('Введіть символи'))];
+
+function findDelete(arrStr, arrSym) {
+  const mutatedArrStr = [...arrStr];
+  for (let i = 0; i < mutatedArrStr.length; i += 1) {
+    for (let j = 0; j < arrSym.length; j += 1) {
+      if (mutatedArrStr[i] === arrSym[j]) {
+        mutatedArrStr.splice(i, 1);
+        i -= 1;
+      }
+    }
+  }
+  return mutatedArrStr.join('');
+}
+
+console.log(findDelete(arreyString, arrayDelSymbol));

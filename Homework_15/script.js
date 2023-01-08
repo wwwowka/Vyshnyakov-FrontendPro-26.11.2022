47ca4be59827387b30c3bb924bd5e0b5043b2eae
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-01-08 15:16:38 +0200

    --amend

--- Homework_15/script.js
+++
@@ -1,9 +0,0 @@
-const num = +prompt('Enter number:');
-const degree = +prompt('Enter degree:');
-function calcPow(num, degree) {
-  if (degree === 1) {
-    return num;
-  }
-  return num * calcPow(num, degree - 1);
-}
-alert(calcPow(num, degree));

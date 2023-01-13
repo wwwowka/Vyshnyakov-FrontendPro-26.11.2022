44610342881746d77811edd680b01fb0f6afd3fd
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-01-13 13:01:03 +0200

    different sorting options

--- Homework_11/script.js
+++ Homework_11/script.js
@@ -2,7 +2,6 @@
 const userLenghtArray = +prompt('Введіть довжену массиву, числом');
 
 const userArray = new Array(userLenghtArray);
-
 console.log(userArray);
 
 for (let i = 0; i < userLenghtArray; i += 1) {
@@ -10,7 +9,19 @@ for (let i = 0; i < userLenghtArray; i += 1) {
   console.log(userArray);
 }
 
-console.log(userArray.sort((a, b) => a - b));
+/* console.log(userArray.sort((a, b) => a - b)); */
+// ['aaa', 'a', '5', '50', '00', '000', '000000000', '00', '00000', '111111111']
+// ['aaa', 'a', '00', '000', '000000000', '00', '00000', '5', '50', '111111111']
+
+/* console.log(userArray.sort((a, b) => (Number.isNaN(+a) || Number.isNaN(+b)
+  ? a.localeCompare(b)
+  : a - b))); */
+// ['aaa', 'a', '5', '50', '00', '000', '000000000', '00', '00000', '111111111']
+// ['00', '000', '000000000', '00', '00000', '5', '50', '111111111', 'a', 'aaa']
+
+console.log(userArray.sort((a, b) => (b < a) - (a < b)));
+/* ['aaa', 'a', '5', '50', '00', '000', '000000000', '00', '00000', '111111111']
+   ['00', '00', '000', '00000', '000000000', '111111111', '5', '50', 'a', 'aaa'] */
 
 console.log(userArray.splice(1, 3));
 

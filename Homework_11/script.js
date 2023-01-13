40c1132d6eddaee2714b533331f507aa6fbdc21e
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-01-13 10:37:21 +0200

    fix sort bug hw11

--- Homework_11/script.js
+++ Homework_11/script.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-alert */
 const userLenghtArray = +prompt('Введіть довжену массиву, числом');
 
 const userArray = new Array(userLenghtArray);
 
 console.log(userArray);
 
-for (let i = 0; i < userLenghtArray; i++) {
+for (let i = 0; i < userLenghtArray; i += 1) {
   userArray[i] = (prompt(`Введіть ${i}-й елемент массиву: текст або число`));
   console.log(userArray);
 }
 
-console.log(userArray.sort());
+console.log(userArray.sort((a, b) => a - b));
 
 console.log(userArray.splice(1, 3));
 

a4907b094cde82a02ab3ae6338f5c811fbd997ad
Volodymyr Vyshniakov <wwwowka@gmail.com>
2023-03-14 16:35:23 +0200

    SW

--- HW_28_Star_Wars/index.js
+++ HW_28_Star_Wars/index.js
@@ -1,111 +1,105 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-use-before-define */
-const output = document.getElementById('output');
-const input = document.getElementById('input');
-const startBtn = document.getElementById('start');
-const stopBtn = document.getElementById('stop');
-const resetBtn = document.getElementById('reset');
-const cursorElement = document.createElement('span');
-const progressBar = document.getElementById('progress-bar');
-cursorElement.innerText = 's';
+const planets = document.getElementById('planets');
+const pagination = document.getElementById('pagination');
+const residents = document.getElementById('residents');
+const vehicles = document.getElementById('vehicles');
+const urlApi = 'https://swapi.dev/api/planets/';
 
-let refreshInterval = null;
-let setSeconds = 0;
-let seconds = input.value;
+async function getApiData(url) {
+  return fetch(url).then((response) => response.json());
+}
 
-// 598 -> 6m 38s; 128 -> 1m 28s
-const convStartTime = (second) => {
-  const arrSeconds = [...second.toString().split('').reverse()];
-  if (arrSeconds[1] > 5) {
-    arrSeconds[1] -= 6;
-    arrSeconds[2] ?? (arrSeconds[2] = 0);
-    arrSeconds[2] = +arrSeconds[2] + 1;
+function createPagination(result) {
+  pagination.innerHTML = '';
+  const totalPages = Math.ceil(result.count / result.results.length);
+  const currentPage = result.next ? (result.next[result.next.length - 1] - 1) : totalPages;
+  for (let i = 1; i <= totalPages; i++) {
+    const pageLink = document.createElement('a');
+    pageLink.innerText = i;
+    pagination.appendChild(pageLink);
+    if (i === currentPage) { pageLink.classList.add('active'); }
+    pageLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      loadPlanets(`${urlApi}?page=${i}`);
+      pagination.innerHTML = '';
+    });
   }
-
-  return [arrSeconds[0] ?? '  ', (arrSeconds[1] ?? '  '), (arrSeconds[2] ?? '  '), (arrSeconds[3] ?? '  ')];
-};
-
-// sec = s + m * 60
-function setSec(arr) {
-  return +arr[0] + +arr[1] * 10 + +arr[2] * 60 + +arr[3] * 60;
 }
 
-function render(second, divider) {
-  const minutes = Math.floor(second / divider);
-  let remainingSeconds = second % divider;
-  if (remainingSeconds < 10) {
-    remainingSeconds = `0${remainingSeconds}`;
-  }
-
-  output.innerText = `${minutes}m ${remainingSeconds}`;
-  output.appendChild(cursorElement);
-
-  document.title = `${minutes}m ${remainingSeconds}s`;
-
-  const progress = (1 - (second / setSec(convStartTime(input.value)))) * 100;
-  progressBar.style.width = `${progress}%`;
+function loadPlanets(url) {
+  planets.innerHTML = '';
+  residents.innerHTML = '';
+  vehicles.innerHTML = '';
+  getApiData(url).then((result) => {
+    result.results.forEach((obj) => {
+      const planetLink = document.createElement('a');
+      const planetPopul = document.createElement('span');
+      const mybr = document.createElement('br');
+      planetLink.innerText = obj.name;
+      planetPopul.innerText = ` | ${obj.population}`;
+      planetLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        loadResidents(`${obj.url}`);
+      });
+      planets.appendChild(planetLink);
+      planets.appendChild(planetPopul);
+      planets.appendChild(mybr);
+    });
+    createPagination(result);
+  });
 }
 
-render(setSec(convStartTime(input.value)), 60);
-progressBar.style.width = '0%';
+loadPlanets(urlApi);
 
-function startCountdown() {
-  progressBar.style.backgroundColor = '#4CAF50';
-  if (setSeconds === 0) {
-    setSeconds = setSec(convStartTime(seconds));
-  }
-  refreshInterval = setInterval(() => {
-    setSeconds -= 1;
-    render(setSeconds, 60);
-    startBtn.style.display = 'none';
-    stopBtn.style.display = 'inline';
-    if (setSeconds === 0) {
-      stopCountdown();
-      progressBar.style.backgroundColor = 'red';
-    }
-  }, 1000);
+function loadResidents(url) {
+  getApiData(url).then((data) => {
+    const residentsList = document.createElement('ul');
+    const residentPromises = data.residents.map((residentUrl) => getApiData(residentUrl));
+    Promise.all(residentPromises).then((residentsData) => {
+      residentsData.forEach((residentData) => {
+        const residentName = residentData.name;
+        const homeworldUrl = residentData.homeworld;
+        getApiData(homeworldUrl).then((homeworldData) => {
+          const residentHomeworld = homeworldData.name;
+          const residentVehiclesCount = residentData.vehicles.length;
+          const carItem = document.createElement('li');
+          const carsLink = document.createElement('a');
+          const homeworldName = document.createElement('span');
+          carItem.innerText = `${residentName} |`;
+          carsLink.innerText = ` ${residentVehiclesCount} `;
+          homeworldName.innerText = `| ${residentHomeworld}`;
+          residentsList.appendChild(carItem);
+          carItem.appendChild(carsLink);
+          carItem.appendChild(homeworldName);
+          carsLink.addEventListener('click', (event) => {
+            event.preventDefault();
+            loadCars([...residentData.vehicles]);
+          });
+        });
+      });
+    });
+    vehicles.innerHTML = '';
+    residents.innerHTML = '';
+    residents.appendChild(residentsList);
+  });
 }
 
-function stopCountdown() {
-  clearInterval(refreshInterval);
-  startBtn.style.display = 'inline';
-  stopBtn.style.display = 'none';
+function loadCars(arr) {
+  const carsList = document.createElement('ul');
+  const backButton = document.createElement('button');
+  backButton.innerText = '< Only Planets';
+  backButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    loadPlanets(urlApi);
+  });
+  const carsPromises = arr.map((carUrl) => getApiData(carUrl));
+  Promise.all(carsPromises).then((carsData) => {
+    carsData.forEach((carData) => {
+      const carItem = document.createElement('li');
+      carItem.innerHTML = `${carData.name} | ${carData.max_atmosphering_speed} | ${carData.cost_in_credits}`;
+      carsList.appendChild(carItem);
+    });
+    vehicles.innerHTML = '';
+    vehicles.appendChild(backButton);
+    vehicles.appendChild(carsList);
+  });
 }
-
-function resetCountdown() {
-  stopCountdown();
-  setSeconds = setSec(convStartTime(input.value));
-  render(setSeconds, 60);
-}
-
-input.addEventListener('input', () => {
-  input.value = input.value.replace(/\D+/g, '');
-  render(input.value, 100);
-  setSeconds = setSec(convStartTime(input.value));
-  seconds = input.value;
-});
-
-input.addEventListener('focus', function () {
-  this.select();
-  output.style.cssText = 'color: #cccccc;';
-  cursorElement.className = 'cursor';
-});
-
-input.addEventListener('keyup', ({ key }) => {
-  if (key === 'Enter') {
-    startCountdown();
-    input.blur();
-  }
-});
-
-startBtn.addEventListener('click', startCountdown);
-stopBtn.addEventListener('click', stopCountdown);
-resetBtn.addEventListener('click', resetCountdown);
-output.addEventListener('click', () => {
-  input.focus();
-});
-
-input.addEventListener('blur', () => {
-  output.style.color = '#000';
-  cursorElement.classList.remove('cursor');
-});
